ca0e606e0644d2d89163ca676a46d69fc5946741
Aditya Poojary <adityapoojary07@gmail.com>
2025-10-07 13:01:54 +0530

    Initialize Maven project with JUnit 5 setup and basic test case

---
+++ Maven with Junit 5/src/test/java/com/telusko/learning/TestTrial.java
@@ -0,0 +1,13 @@
+package com.telusko.learning;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTrial {
+
+    @Test
+    void test() {
+        assertEquals(6, 6);
+    }
+}
